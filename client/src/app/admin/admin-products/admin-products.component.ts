da1f44f993469c61e7cfa48f509409f69c65ace8
Gal <galpalas89@gmail.com>
2022-02-03 15:06:06 +0200

    Implement updating a product

--- client/src/app/admin/admin-products/admin-products.component.ts
+++ client/src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/shared/Product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.css']
+  styleUrls: ['./admin-products.component.css'],
 })
 export class AdminProductsComponent implements OnInit {
+  products$: Product[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private productService: ProductService) {
+    this.productService.getProducts().subscribe((product) => {
+      this.products$ = product;
+    });
   }
 
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((product) => {
+      this.products$ = product;
+    });
+  }
 }
--- client/src/app/admin/product-form/product-form.component.ts
+++ client/src/app/admin/product-form/product-form.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/shared/Product';
 import { Component, OnInit } from '@angular/core';
 import { Categories } from 'src/app/shared/Categories';
@@ -10,23 +11,41 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-form.component.css'],
 })
 export class ProductFormComponent implements OnInit {
+  id;
   categories: Categories[] = [];
   products: Product[] = [];
+  product = {} as Product;
 
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
     private categoryService: CategoryService,
     private productService: ProductService
   ) {
+    this.id = this.route.snapshot.paramMap.get('id');
+
     this.categoryService.getCategories().subscribe((category) => {
       this.categories = category;
     });
+
+    if (this.id) {
+      this.productService.getProductById(this.id).subscribe((product) => {
+        this.product = product;
+      });
+    }
   }
 
   ngOnInit(): void {}
 
   save(product: Product) {
-    this.productService.postProduct(product).subscribe((product) => {
-      this.products.push(product);
-    });
+    if (this.id) {
+      this.productService.putProduct(this.id, product).subscribe();
+    } else {
+      this.productService.postProduct(product).subscribe((product) => {
+        this.products.push(product);
+      });
+    }
+
+    this.router.navigate(['/admin/products']);
   }
 }
